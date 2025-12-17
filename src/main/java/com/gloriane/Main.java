5bff8d357ccd3615c8ca8b5140bac1c3830bf48d
Glory Ngum Foncha <gloriane7767@gmail.com>
2025-12-17 10:03:20 +0100

    create customer class with all methods

--- src/main/java/com/gloriane/Main.java
+++ src/main/java/com/gloriane/Main.java
@@ -2,6 +2,8 @@ package com.gloriane;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Welcome to the Gloriane Application!");
+        Customer customer = new Customer("Example User", "test.test@example.com");
+        System.out.println(customer.getCustomer());
     }
 }
