11f4c5e535d8e8ae8dd60b6cd57a36a4298774d8
Glory Ngum Foncha <gloriane7767@gmail.com>
2025-12-17 11:18:04 +0100

    update toString and get method for customer and product classes

--- src/main/java/com/gloriane/Main.java
+++ src/main/java/com/gloriane/Main.java
@@ -5,5 +5,9 @@ public class Main {
         System.out.println("Welcome to the Gloriane Application!");
         Customer customer = new Customer("Example User", "test.test@example.com");
         System.out.println(customer.getCustomer());
+
+
+        Product product = new Product("Example Product", 29.99);
+        System.out.println(product.getProduct());
     }
 }
